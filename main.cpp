8c0ea474195f83c35d28e8c326b604eaadc78dbc
MariuszGruszczynski <78258950+MariuszGruszczynski@users.noreply.github.com>
2021-11-09 00:21:11 +0100

    PO1_LAB4_V1

--- main.cpp
+++ main.cpp
@@ -4,197 +4,130 @@
 
 using namespace std;
 
-class Klasa {
-private:
-    int x;
-    int y;
-    int z;
+class Pracownik {
+protected:
+    string imie;
+    string nazwisko;
+    string nip;
+    string dataUr;
+    string dzial;
+    int pensja;
 
 public:
-    Klasa() {
-        setX(0);
-        setY(0);
-        setZ(0);
-        puts("konstruktor domyslny");
+    Pracownik() = delete;
+
+    Pracownik(string imie, string nazwisko, string nip, string dataUr, string dzial, int pensja):
+        imie(imie),
+        nazwisko(nazwisko),
+        nip(nip),
+        dataUr(dataUr),
+        dzial(dzial),
+        pensja(pensja) {
+
     }
 
-    Klasa(int x) {
-        setX(x);
-        setY(0);
-        setZ(0);
-        printf("konstruktor z jednym argumentem x = %d\n", getX());
+    virtual ~Pracownik() {
+
     }
 
-    Klasa(int x, int y) {
-        setX(x);
-        setY(y);
-        setZ(0);
-        printf("konstruktor z dwoma argumentami x = %d y = %d\n", getX(), getY());
-    }
-
-    Klasa(int x, int y, int z) {
-        setX(x);
-        setY(y);
-        setZ(z);
-        printf("konstruktor z trzema argumentami x = %d y = %d z = %d\n", getX(), getY(), getZ());
-    }
-
-    Klasa(const Klasa& k) {
-        puts("konstruktor kopiujacy");
-        setX(k.getX());
-        setY(k.getY());
-        setZ(k.getZ());
-    }
-
-    int getX() const;
-    int getY() const;
-    int getZ() const;
-    void setX(int x);
-    void setY(int y);
-    void setZ(int z);
+    virtual void display();
 };
 
-void Klasa::setX(int x) {
-    this->x = x;
+void Pracownik::display() {
+    cout << imie << " " << nazwisko << " nip: " << nip << " data: " << dataUr << " dzial: " << dzial << " pensja: " << pensja;
 }
 
-void Klasa::setY(int y) {
-    this->y = y;
-}
-
-void Klasa::setZ(int z) {
-    this->z = z;
-}
-
-
-int Klasa::getX() const {
-    return x;
-}
-
-
-int Klasa::getY() const {
-    return y;
-}
-
-
-int Klasa::getZ() const {
-    return z;
-}
-
-
-class Pkt {
-public:
-    int x;
-    int y;
-
-    Pkt():
-        x(0),
-        y(0) {
-
-    }
-
-    Pkt(int x, int y):
-        x(x),
-        y(y) {
-
-    }
-
-    void set(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-};
-
-class Wektor {
-private:
-    static int count;
+class Kierownik : public Pracownik {
+protected:
+    string typKierownictwa;
+    int dodatek; //wyrazony w %
+    int iloscPodwladnych;
 
 public:
-    Pkt pocz, kon;
+    Kierownik() = delete;
 
-    Wektor() {
-        count++;
+    Kierownik(string imie, string nazwisko, string nip, string dataUr, string dzial, int pensja, string typKierownictwa, int dodatek, int iloscPodwladnych):
+        Pracownik(imie, nazwisko, nip, dataUr, dzial, pensja),
+        typKierownictwa(typKierownictwa),
+        dodatek(dodatek),
+        iloscPodwladnych(iloscPodwladnych) {
+
     }
 
-    Wektor(Pkt pocz, Pkt kon):
-        pocz(pocz),
-        kon(kon) {
-        count++;
+    virtual ~Kierownik() {
+
     }
 
-    Wektor(int poczX, int poczY, int konX, int konY) {
-        pocz.x = poczX;
-        pocz.y = poczY;
-        kon.x = konX;
-        kon.y = konY;
-        count++;
-    }
-
-    ~Wektor() {
-        puts("zadanie wykonane");
-    }
-
-    void dodaj(const Wektor&);
-    void odejmij(const Wektor&);
-    static int mnozenieSkalarne(const Wektor&, const Wektor&);
-    static int getCount();
-    void display();
+    virtual void display() override;
 };
 
-void Wektor::display() {
-    printf("PoczatekX: %d, PoczatekY: %d, KoniecX: %d, KoniecY: %d\n", pocz.x, pocz.y, kon.x, kon.y);
+void Kierownik::display() {
+    Pracownik::display();
+    cout << " typ kierownictwa: " << typKierownictwa << " dodatek: " << dodatek << " ilosc podwladnych: " << iloscPodwladnych;
 }
 
-void Wektor::dodaj(const Wektor& w) {
-    this->pocz.x += w.pocz.x;
-    this->pocz.y += w.pocz.y;
-    this->kon.x += w.kon.x;
-    this->kon.y += w.kon.y;
+class Resistor {
+    double res;
+
+public:
+    Resistor():
+        res(0) {
+
+    }
+
+    Resistor(double res):
+        res(res) {
+
+    }
+
+    double r() const;
+    void r(double);
+
+    friend Resistor operator+(const Resistor&, const Resistor&);
+    friend Resistor operator*(const Resistor&, const Resistor&);
+    friend ostream& operator<<(ostream&, const Resistor&);
+};
+
+double Resistor::r() const {
+    return res;
 }
 
-void Wektor::odejmij(const Wektor& w) {
-    this->pocz.x -= w.pocz.x;
-    this->pocz.y -= w.pocz.y;
-    this->kon.x -= w.kon.x;
-    this->kon.y -= w.kon.y;
+void Resistor::r(double r) {
+    res = r;
 }
 
-int Wektor::mnozenieSkalarne(const Wektor &a, const Wektor &b) {
-    int aDlugoscX = a.kon.x - a.pocz.x;
-    int aDlugoscY = a.kon.y - a.pocz.y;
-    int bDlugoscX = b.kon.x - b.pocz.x;
-    int bDlugoscY = b.kon.y - b.pocz.y;
+Resistor operator+(const Resistor& r1, const Resistor& r2) {
+    Resistor ret(r1.r() + r2.r());
 
-    return aDlugoscX * bDlugoscX + aDlugoscY * bDlugoscY;
+    return ret;
 }
 
-int Wektor::getCount() {
-    return count;
+Resistor operator*(const Resistor& r1, const Resistor& r2) {
+    Resistor ret((r1.r() + r2.r()) / (r1.r() * r2.r()));
+
+    return ret;
 }
 
-int Wektor::count = 0;
+ostream& operator<<(ostream& os, const Resistor& res) {
+    os << res.r();
+    return os;
+}
 
 int main() {
-    Wektor a(2, 1, 3, 4);
-    Wektor b(5, 2, 7, 5);
+    //zad 2
 
-    a.display();
-    b.display();
-    a.dodaj(b);
-    a.display();
-    a.odejmij(b);
-    a.display();
-    puts("");
-    printf("mnozenie skalarne: %d\n", Wektor::mnozenieSkalarne(a, b));
-    printf("ile wektorow?: %d\n", Wektor::getCount());
-    puts("");
 
-    Klasa q, w(4), e(1, 4), r(5, 2, 1);
-    puts("");
 
-    Klasa t(r);
-    printf("skopiowana klasa: %d %d %d\n", t.getX(), t.getY(), t.getZ());
-    puts("");
+    //zad 3
+    Resistor r1(5), r2(8);
+
+    Resistor r3 = r1 + r2;
+
+    cout << r3 << endl;
+
+    r3 = r1 * r2;
+
+    cout << r3 << endl;
 
     return 0;
 }
