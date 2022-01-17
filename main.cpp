190e321cba3b81ff3a7bf46624024da4599f6eda
MariuszGruszczynski <78258950+MariuszGruszczynski@users.noreply.github.com>
2022-01-17 15:27:04 +0100

    PO1_LAB6_ZAD1

--- main.cpp
+++ main.cpp
@@ -1,135 +1,60 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstdio>
+#include <stdio.h>
+#include <string>
 
-#define M_PI 3.14159265358979323846
+using namespace std;
 
-using std::cout;
-using std::endl;
-
-class Bryla {
+template<class TYPE> class Fibonacci {
 public:
-    Bryla() {
+    TYPE fib(const TYPE& nthElement) {
+        if(nthElement < 3.0)
+            return 1;
 
-    }
-    virtual ~Bryla() {
-
-    }
-
-    virtual float Pole() {
-        return 0;
-    }
-    virtual float Objetosc() {
-        return 0;
-    }
-    virtual float Przekatna() {
-        return 0;
+        return fib(nthElement - 1) + fib(nthElement - 2);
     }
 };
 
-class Ostroslup: public Bryla { //zakladamy ze w podstawie jest wielokat foremny
-private:
-    int iloscKatowPodstawy;
-    float dlugoscBokuPodstawy, promienOkreguWpisanego;
-    float wysokoscOstroslupa, wysokoscScianyBocznej;
-public:
-    Ostroslup(int iloscKatowPodstawy, float dlugoscBokuPodstawy, float promienOkreguWpisanego, float wysokoscOstroslupa, float wysokoscScianyBocznej):
-        Bryla(),
-        iloscKatowPodstawy(iloscKatowPodstawy),
-        dlugoscBokuPodstawy(dlugoscBokuPodstawy),
-        promienOkreguWpisanego(promienOkreguWpisanego),
-        wysokoscOstroslupa(wysokoscOstroslupa),
-        wysokoscScianyBocznej(wysokoscScianyBocznej) {
+template<class TYPE>
+bool czyFib(TYPE liczba) {
+    TYPE a = 1.0;
+    TYPE b = 1.0;
 
+    while (liczba != a && liczba != b) {
+        a += b;
+        b += a;
+        if (b > liczba)
+            break;
     }
 
-    virtual float Objetosc();
-    virtual float Pole();
-};
+    if(liczba == b || liczba == a)
+        return true;
 
-float Ostroslup::Objetosc() {
-    float Pp = iloscKatowPodstawy * dlugoscBokuPodstawy * promienOkreguWpisanego / 2.0; // pole wielokata foremnego = nar/2
-
-    return 1.0 / 3.0 * Pp * wysokoscOstroslupa;
-}
-
-float Ostroslup::Pole() {
-    float Pp = iloscKatowPodstawy * dlugoscBokuPodstawy * promienOkreguWpisanego / 2.0; // pole wielokata foremnego = nar/2
-    float poleScianyBocznej = dlugoscBokuPodstawy * wysokoscScianyBocznej / 2.0;
-    float Pb = iloscKatowPodstawy * poleScianyBocznej;
-
-    return Pp + Pb;
-}
-
-
-class Prostopadloscian: public Bryla {
-private:
-    float width, height, depth;
-
-public:
-    Prostopadloscian(float width, float height, float depth):
-        Bryla(),
-        width(width),
-        height(height),
-        depth(depth) {}
-
-    virtual float Objetosc();
-    virtual float Pole();
-};
-
-float Prostopadloscian::Objetosc() {
-    return width * height * depth;
-}
-
-float Prostopadloscian::Pole() {
-    return 2 * (width * height + width * depth + height * depth);
-}
-
-
-class Kula: public Bryla {
-private:
-    float r; //promien w cm
-
-public:
-    Kula(float r):
-        Bryla(),
-        r(r) {
-
-    }
-
-    virtual ~Kula() {
-
-    }
-
-    virtual float Pole();
-    virtual float Objetosc();
-};
-
-float Kula::Pole() {
-    return 4.0 * M_PI * r * r;
-}
-
-float Kula::Objetosc() {
-    return 4.0 / 3.0 * M_PI * r * r * r;
+    return false;
 }
 
 int main() {
-    Bryla *kula = new Kula(1);
+    Fibonacci<int> fib_int;
+    Fibonacci<float> fib_float;
+    Fibonacci<long long int> fib_lli;
 
-    cout << "Kula: " << kula->Pole() << " cm^2 " << kula->Objetosc() << " cm^3" << endl;
+    int i = 0;
+    cout << "int: ";
+    cin >> i;
+    cout << "wynik ciagu fibonacciego dla int: " << fib_int.fib(i) << endl;
 
-    Bryla *prosto = new Prostopadloscian(5, 3, 4);
+    float f = 0.0;
+    cout << "float: ";
+    cin >> f;
+    cout << "wynik ciagu fibonacciego dla float: " << fib_float.fib(f) << endl;
 
-    cout << "Prostopadloscian: " << prosto->Pole() << " cm^2 " << prosto->Objetosc() << " cm^3" << endl;
+    cout << "long long int: ";
+    long long int lli = 0;
+    cin >> lli;
+    cout << "wynik ciagu fibonacciego dla long long int: " << fib_lli.fib(lli) <<  endl << endl;
 
-    Bryla *ostro = new Ostroslup(5, 3.5, 8, 4, 3.5);
-
-    cout << "Ostroslup: " << ostro->Pole() << " cm^2 " << ostro->Objetosc() << " cm^3" << endl;
-
-
-    delete ostro;
-    delete kula;
-    delete prosto;
+    cout << "czy liczba 90 nalezy do ciagu? " << (czyFib(90) ? "tak" : "nie") << endl;
+    cout << "czy liczba 18.0 nalezy do ciagu? " << (czyFib(18.0) ? "tak" : "nie") << endl;
+    cout << "czy liczba 5555 nalezy do ciagu? " << (czyFib(832040) ? "tak" : "nie") << endl;
 
     return 0;
 }
