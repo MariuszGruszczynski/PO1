8da5096240aea0aac8470b3ee1cef2271236e18e
MariuszGruszczynski <mariuszgruszczynski11@gmail.com>
2022-01-22 22:40:34 +0100

    PO1_LAB9_ZAD2
    Napisz klasę macierz, która służy do przechowywania macierzy liczb wymiernych. Klasa macierz powinna udostępniać następujące publiczne metody: — konstruktor, który otrzymuje dwie liczby całkowite i inicjuje macierz o wymiarach podanych w argumentach, — przeciążony operator (), który dla liczb całkowitych i, j podanych jako argumenty zwraca referencję do elementu macierzy znajdującego się i-tej kolumnie i j-wierszu. Do implementacji klasy macierz wykorzystaj typ vector.

--- main.cpp
+++ main.cpp
@@ -1,65 +1,60 @@
 #include <iostream>
-#include <list>
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
+using std::to_string;
+
+//indeksy liczone od 1
+class Macierz final {
+    int width, height;
+    std::vector<float> data;
+
+public:
+    Macierz(int _width, int _height):
+        width(_width),
+        height(_height) {
+        data.reserve(width * height);
+
+        for(int i = 0; i < width * height; i++)
+            data[i] = i + 1 + i / 10.0;
+    }
+
+    ~Macierz() {
+        data.clear();
+    }
+
+    float& operator()(int i, int j) throw(std::out_of_range) { //wyrzuca std::out_of_range
+        if(i - 1 >= width || i < 1 || j - 1 >= height || j < 1) {
+            string exception_str = "parametry i: " + to_string(i) + " oraz j: " + to_string(j) + " przy szerokosci: "  + to_string(width) + " oraz wysokosci: " + to_string(height);
+            throw std::out_of_range(exception_str);
+        }
+
+        return data[(j - 1) * width + i - 1];
+    }
+
+    void print() {
+        for(int i = 0; i < height ; i++) {
+            for(int j = 0; j < width; j++) {
+                cout << data[width * i + j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
 
 int main(void) {
-    int n_liczb = -1;
-
-    cout << "Wczytywanie ilosci liczb..." << endl;
-    do {
-        cout << "podaj liczbe dodatnia: ";
-        cin >> n_liczb;
-
-        if(cin.fail()) {
-            cout << "podano bledne dane, sprobuj ponownie" << endl;
-            cin.clear();
-            cin.ignore(1000, '\n');
-            continue;
-        }
-
-        if(n_liczb <= 0) {
-            cout << "podano bledne dane, sprobuj ponownie" << endl << endl;
-            continue;
-        }
-
-        break;
-    } while(1);
-
-    std::list<int> Lista;
-
-    cout << endl << "Wczytywanie kolejnych liczb..." << endl;
-
-    for(int i = 0; i < n_liczb; i++) {
-        int temp = 0, temp2 = 0;
-
-        cout << "podaj pare liczb: ";
-        cin >> temp >> temp2;
-
-        if(cin.fail()) {
-            cout << "podano bledne dane, sprobuj ponownie" << endl;
-            cin.clear();
-            cin.ignore(1000, '\n');
-            i--;
-            continue;
-        }
-
-        if(temp < 0)
-            Lista.push_front(temp);
-        else
-            Lista.push_back(temp);
-        if(temp2 < 0)
-            Lista.push_front(temp2);
-        else
-            Lista.push_back(temp2);
-    }
+    Macierz m(3, 4);
+    m.print();
 
     cout << endl;
-    for(auto &a : Lista){
-        cout << a << endl;
-    }
+
+    m(2,2) = 5.42;
+    m(1,1) = 78.12;
+    m(3,4) = 1005.22;
+    m.print();
 
     return 0;
 }
