a9d8d652d6959cdd6e5a0ab6b93da16b2b9effae
MariuszGruszczynski <mariuszgruszczynski11@gmail.com>
2022-01-23 22:41:21 +0100

    PO1_LAB8_ZAD2

--- main.cpp
+++ main.cpp
@@ -1,61 +1,140 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
+#include <cmath>
+#define M_PI 3.14159265358979323846
 
 using std::cout;
 using std::endl;
 using std::ostream;
 
-class Kondensator {
+class Wektor3D {
 private:
-    double poj;
+    double x, y, z;
 
 public:
-    Kondensator() : poj(0) {
+    Wektor3D(): x(0),
+        y(0),
+        z(0) {
     }
 
-    Kondensator(double poj) : poj(poj) {
+    Wektor3D(double x, double y, double z):
+        x(x),
+        y(y),
+        z(z) {
+
     }
 
-    double pojemnosc() {
-        return poj;
+    Wektor3D operator-() const {
+        Wektor3D w(-x, -y, -z);
+
+        return w;
     }
 
-    void pojemnosc(double p) {
-        poj = p;
+    Wektor3D operator*(const double &a) {
+        Wektor3D w(x * a, y * a, z * a);
+
+        return w;
     }
 
-    friend Kondensator operator+(const Kondensator &, const Kondensator &);
-    friend Kondensator operator*(const Kondensator &, const Kondensator &);
-    friend ostream &operator<<(ostream &, const Kondensator &);
+    friend Wektor3D operator*(const double &a, Wektor3D &w);
+
+    void print() {
+        cout << "x: " << x << ", y: " << y << ", z: " << z << endl;
+    }
+
+    friend double katPomiedzyWektorami(const Wektor3D &w1, const Wektor3D &w2);
+    friend Wektor3D operator+(const Wektor3D &w1, const Wektor3D &w2);
+    friend Wektor3D operator-(const Wektor3D &w1, const Wektor3D &w2);
+    friend double operator*(const Wektor3D &w1, const Wektor3D &w2);
+    friend Wektor3D operator/(const Wektor3D &w1, const Wektor3D &w2);
+    friend ostream& operator<<(ostream& os, const Wektor3D &w);
+
+
+
+private:
+    double dlugoscWektora() const {
+        double _dlugoscKwadrat = x * x + y * y + z * z;
+        double _dlugosc = sqrt(_dlugoscKwadrat);
+
+        return _dlugosc;
+    }
+
+    static double iloczynSkalarny(const Wektor3D &w1, const Wektor3D &w2) {
+        double _iloczyn = w1.x * w2.x + w1.y * w2.y + w1.z * w2.z;
+
+        return _iloczyn;
+    }
 };
 
-ostream &operator<<(ostream &os, const Kondensator &k) {
-    os << "Kondensator o pojemnosci: " << k.poj << endl;
+ostream& operator<<(ostream& os, const Wektor3D &w) {
+    cout << "x: " << w.x << ", y: " << w.y << ", z: " << w.z;
     return os;
 }
 
-Kondensator operator+(const Kondensator &k1, const Kondensator &k2) { // + czyli szeregowy czyli dodawanie odwrotnosci
-    double reverseC = (1.0 / k1.poj) + (1.0 / k2.poj);
-    double C = 1.0 / reverseC;
+double katPomiedzyWektorami(const Wektor3D &w1, const Wektor3D &w2) {
+    double _katRadiany = acos(Wektor3D::iloczynSkalarny(w1, w2) / (w1.dlugoscWektora() * w2.dlugoscWektora()));
+    double _katStopnie = _katRadiany * 180.0 / M_PI;
 
-    Kondensator k(C);
-
-    return k;
+    return _katStopnie;
 }
 
-Kondensator operator*(const Kondensator &k1, const Kondensator &k2) { // * czyli rownolegly czyli zwykle dodawanie
-    Kondensator k(k1.poj + k2.poj);
-    return k;
+Wektor3D operator*(const double &a, Wektor3D &w) {
+    Wektor3D newWektor = w * a;
+
+    return newWektor;
+}
+
+Wektor3D operator+(const Wektor3D &w1, const Wektor3D &w2) {
+    Wektor3D w(w1.x + w2.x, w1.y + w2.y, w1.z + w2.z);
+
+    return w;
+}
+
+double operator*(const Wektor3D &w1, const Wektor3D &w2) {
+    double w = Wektor3D::iloczynSkalarny(w1, w2);
+
+    return w;
+}
+
+Wektor3D operator-(const Wektor3D &w1, const Wektor3D &w2) {
+    Wektor3D w(w1.x - w2.x, w1.y - w2.y, w1.z - w2.z);
+
+    return w;
+}
+
+Wektor3D operator/(const Wektor3D &w1, const Wektor3D &w2) {
+    Wektor3D w;
+
+    w.x = w1.y * w2.z - w2.y * w1.z;
+    w.y = w2.x * w1.z - w1.x * w1.z;
+    w.z = w1.x * w2.y - w2.x * w1.y;
+
+    return w;
 }
 
 int main() {
-    Kondensator k1(4), k2(5), k3(6), k4(7);
-    Kondensator kMnozenie = k1 * k2 * k3 * k4;
-    Kondensator kDodawanie = k1 + k2 + k3 + k4;
+    Wektor3D w(1.23, 2.42, 3.63465);
+    Wektor3D minusw = -w;
 
-    cout << kMnozenie;  //mnozenie, laczenie rownolegle
-    cout << kDodawanie; //dodawanie, laczenie szeregowe
+    minusw.print();
+
+    Wektor3D multiply = 2.42 * w;
+    multiply.print();
+
+    Wektor3D aaa(-2, 1, 2);
+    Wektor3D bbb(0, 1, 1);
+
+    cout << "kat pomiedzy wektorami: " << katPomiedzyWektorami(aaa, bbb) << " stopni" << endl;
+    cout << "iloczyn skalarny: " << aaa*bbb << endl;
+    cout << "iloczyn skalarny (2): " << w*multiply << endl;
+
+    printf("iloczyn wektorowy: ");
+    (aaa / bbb).print();
+    printf("iloczyn wektorowy (2): ");
+    (bbb / aaa).print();
+
+    cout << aaa << endl << bbb << endl;
 
     return 0;
 }
