addb91e03bbe9d48a50aa48713829f84070b9e5e
MariuszGruszczynski <mariuszgruszczynski11@gmail.com>
2022-01-23 22:40:15 +0100

    PO1_LAB7_ZAD3

--- main.cpp
+++ main.cpp
@@ -8,85 +8,43 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::exception;
 
-void fun() {
-    float a = 0, b = 0, c = 0;
-    int type = 0; //(1) - rzeczywiste, (2) - zespolone
+//u¿ycie w³asnej klasy wyj¹tku
 
-    cout << "(1) - rzeczywiste, (2) - zespolone\n";
+class MojWyjatek: public exception {
+public:
+    virtual const char* what() const throw() {
+        return "Moj wyjatek";
+    }
+};
 
-    while(1) {
-        int temp = scanf("%d", &type);
+int main () {
+    MojWyjatek myex;
 
-        if(temp == 0 || (type != 1 && type != 2)) {
-            char c = 0;
-            scanf("%c", &c);
-            cout << "error! try again: ";
-        } else
-            break;
+    try {
+        int x = 0, z = 0;
+
+        cout << "podaj x, z: ";
+        cin >> x >> z;
+
+        if(!cin.good())
+            throw myex;
+
+        if(x * x - z * z < 0)
+            throw myex;
+
+        if(x - z <= 0)
+            throw myex;
+
+        float y = sqrt(x * x - z * z) / sqrt(x - z);
+
+        printf("wynik: %f", y);
     }
 
-    cout << "podaj parametry a, b i c: ";
-    do {
-        cin >> a >> b >> c;
-
-        if(cin.good())
-            break;
-        else {
-            cout << "error! try again: ";
-            cin.clear();
-            cin.ignore(10000, '\n');
-        }
-    } while(1);
-
-    if(type == 1) {
-        try {
-            float delta = b * b - 4 * a * c;
-
-            if(delta < 0)
-                throw std::logic_error("delta mniejsza od zera");
-
-            float sqrtDelta = sqrt(delta);
-
-            if(a == 0)
-                throw std::logic_error("dzielenie przez zero (a=0)");
-
-            if(delta > 0) {
-                float x1 = (-b - sqrtDelta) / (2 * a);
-                float x2 = (-b + sqrtDelta) / (2 * a);
-                printf("dwa rozwiazania rzeczywiste: %f %f\n", x1, x2);
-            } else if(delta == 0) {
-                float x0 = -b / (2 * a);
-                printf("jedno rozwiazanie rzeczywiste: %f\n", x0);
-            }
-        } catch(const std::logic_error &e) {
-            cout << "error: " << e.what() << endl;
-        }
-    } else if(type == 2) {
-        try {
-            float delta = b * b - 4 * a * c;
-
-            if(a == 0)
-                throw std::logic_error("dzielenie przez 0 (a=0)");
-            if(delta >= 0)
-                throw std::logic_error("delta nieujemna");
-
-            delta = -delta;
-            float sqrtDelta = sqrt(delta);
-
-            float real = -b / (2 * a);
-            float imag = sqrtDelta / (2 * a);
-
-            printf("dwa pierwiastki zespolone: %f + %fi oraz %f - %fi\n", real, imag, real, imag);
-
-        } catch(const std::logic_error &e) {
-            cout << "error: " << e.what() << endl;
-        }
+    catch (const exception& e) {
+        cout << e.what() << endl;
     }
-}
-
-int main() {
-    fun();
 
     return 0;
 }
